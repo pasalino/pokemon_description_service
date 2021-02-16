d685bcd14d6c21a6b542e5ed7c81962e40cfb8b4
Pasqualino de Simone <pasalino@gmail.com>
2021-02-16 09:35:25 +0100

    Little refactor in translationService. * Add types for axios stuffs * Insert debug logging in function error

--- src/services/translationService.ts
+++ src/services/translationService.ts
@@ -1,4 +1,5 @@
-import axios from "axios";
+import axios, {AxiosRequestConfig, AxiosResponse} from "axios";
+import logger from "../helpers/logger";
 
 export const FUN_TRANSLATIONS_BASE_URL = "https://api.funtranslations.com";
 export const TRANSLATE_IN_SHAKESPEAREAN_PATH = "/translate/shakespeare";
@@ -12,18 +13,19 @@ export const translateInShakespearean = async (text: string): Promise<string> =>
         const headers = {
             ...(process.env.FUN_TRANSLATION_KEY && {"X-Funtranslations-Api-Secret": process.env.FUN_TRANSLATION_KEY})
         }
-        const timeout = parseInt(process.env.EXTERNAL_API_TIMEOUT, 10) || 0
+        const timeout: number = parseInt(process.env.EXTERNAL_API_TIMEOUT, 10) || 0
 
-        const options = {headers, timeout};
+        const options: AxiosRequestConfig = {headers, timeout};
         const bodyData = {text};
         const url = new URL(TRANSLATE_IN_SHAKESPEAREAN_PATH, FUN_TRANSLATIONS_BASE_URL).href;
 
-        const {data} = await axios.post(url, bodyData, options);
-
+        const response: AxiosResponse = await axios.post(url, bodyData, options);
+        const data = response.data;
         if (data.success.total !== 1) throw new Error(ERROR_TRANSLATE_NOT_UNIQUE);
 
         return data.contents.translated;
     } catch (e) {
+        logger.debug("Error in translateInShakespearean", e);
         if (e.message && e.message.includes("timeout of")) throw new Error("Translation service in timeout")
         if (e.message === ERROR_TRANSLATE_NOT_UNIQUE) throw e;
         if (e.response && e.response.status === 429) throw new Error("Maximus quota reached in translate service");
